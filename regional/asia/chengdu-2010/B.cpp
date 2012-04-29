a941fe2c5e22d583bd434a6865cfaf92e095fb80
ftiasch <ftiasch0@gmail.com>
2012-04-29 16:07:25 +0800

    TCO11 Online Round 2 EASY & MEDIUM

---
+++ regional/asia/chengdu-2010/B.cpp
@@ -0,0 +1,15 @@
+// Chengdu 2010
+// Problem B -- Battle over Cities
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int test_count;
+    scanf("%d", &test_count);
+    while (test_count > 0) {
+        test_count --;
+    }
+    return 0;
+}

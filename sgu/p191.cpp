967f2275ec1b38a031523288c1bdd0f08607f237
ftiasch <ftiasch0@gmail.com>
2012-06-28 17:18:12 +0800

    SGU 191 RGB

--- sgu/p191.cpp
+++ sgu/p191.cpp
@@ -1,33 +1,116 @@
-// SGU 191 -- Exhibition
+// SGU 192 -- RGB
+#include <cmath>
 #include <cstdio>
-#include <iostream>
+#include <cstring>
 #include <string>
+#include <utility>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-vector <int> toVector(const string &s) {
-    vector <int> v;
-    for (int i = (int)s.length() - 1; i >= 0; -- i) {
-        v.push_back(s[i] - 'A');
-    }
-    return v;
+const int N = 300;
+const double EPS = 1e-9;
+const string COLORS = "RGB";
+
+int sgn(double x) {
+    return x < -EPS? -1: x > EPS;
+}
+
+struct Point {
+    double x, y;
+
+    Point(double x = 0, double y = 0): x(x), y(y) {}
+};
+
+Point operator -(const Point &a, const Point &b) {
+    return Point(a.x - b.x, a.y - b.y);
+}
+
+Point operator *(const Point &a, double k) {
+    return Point(a.x * k, a.y * k);
+}
+
+Point operator /(const Point &a, double k) {
+    return Point(a.x / k, a.y / k);
+}
+
+double det(const Point &a, const Point &b) {
+    return a.x * b.y - b.x * a.y;
+}
+
+Point intersect(const Point &a, const Point &b, const Point &c, const Point &d) {
+    double areaABC = det(b - a, c - a);
+    double areaABD = det(b - a, d - a);
+    return (c * areaABD - d * areaABC) / (areaABD - areaABC);
+}
+
+int n, color[N];
+double length[3];
+Point ends[N][2];
+
+double getY(int i, double x_0) {
+    return ends[i][0].y + (x_0 - ends[i][0].x) / (ends[i][1].x - ends[i][0].x) * (ends[i][1].y - ends[i][0].y);
 }
 
 int main() {
-    string a, b;
-    cin >> a >> b;
-    vector <int> init = toVector(a);
-    vector <int> final = toVector(b);
-    while (final.size() >= init.size() && init.size() > 0) {
-        if (final.back() == init.back()) {
-            int type = init.back();
-            init.push_back(type ^ 1);
-        } else {
-            init.pop_back();
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++ i) {
+        for (int j = 0; j < 2; ++ j) {
+            scanf("%lf%lf", &ends[i][j].x, &ends[i][j].y);
         }
-        final.pop_back();
+        if (sgn(ends[i][0].x - ends[i][1].x) > 0) {
+            swap(ends[i][0], ends[i][1]);
+        }
+        char buffer[2];
+        scanf("%s", buffer);
+        color[i] = COLORS.find(*buffer);
     }
-    puts(final.empty() && init.empty()? "YES": "NO");
+    memset(length, 0, sizeof(length));
+    for (int i = 0; i < n; ++ i) {
+        if (sgn(ends[i][0].x - ends[i][1].x) == 0) {
+            continue;
+        }
+        vector <pair <double, double> > intervals;
+        for (int j = 0; j < n; ++ j) {
+            if (i == j || sgn(ends[j][0].x - ends[j][1].x) == 0) {
+                continue;
+            }
+            double begin = ends[j][0].x;
+            double end = ends[j][1].x;
+            if (sgn(det(ends[i][1] - ends[i][0], ends[j][1] - ends[j][0])) == 0) {
+                if (sgn(getY(j, 0) - getY(i, 0)) < 0) {
+                    intervals.push_back(make_pair(begin, end));
+                }
+            } else {
+                Point p = intersect(ends[i][0], ends[i][1], ends[j][0], ends[j][1]);
+                if (sgn(getY(j, p.x - 1) - getY(i, p.x - 1)) < 0 && sgn(begin - p.x) < 0) {
+                    intervals.push_back(make_pair(begin, min(p.x, end)));
+                }
+                if (sgn(getY(j, p.x + 1) - getY(i, p.x + 1)) < 0 && sgn(p.x - end) < 0) {
+                    intervals.push_back(make_pair(max(p.x, begin), end));
+                }
+            }
+        }
+        double left = ends[i][1].x - ends[i][0].x;
+        sort(intervals.begin(), intervals.end());
+        if ((int)intervals.size() > 0) {
+            double begin = intervals.front().first;
+            double end = intervals.front().second;
+            for (int k = 1; k < (int)intervals.size(); ++ k) {
+                if (sgn(intervals[k].first - end) <= 0) {
+                    end = max(end, intervals[k].second);
+                } else {
+                    left -= max(min(end, ends[i][1].x) - max(begin, ends[i][0].x), 0.0);
+                    begin = intervals[k].first;
+                    end = intervals[k].second;
+                }
+            }
+            left -= max(min(end, ends[i][1].x) - max(begin, ends[i][0].x), 0.0);
+        }
+        length[color[i]] += left;
+    }
+    for (int i = 0; i < 3; ++ i) {
+        printf("%c %.2f\n", COLORS[i], length[i]);
+    }
     return 0;
 }

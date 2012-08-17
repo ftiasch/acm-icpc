e50490799cb5d1effc4c200f18c4dce13e6e724a
ftiasch <ftiasch0@gmail.com>
2012-08-17 10:23:59 +0800

    SGU 297 Fair-play

---
+++ sgu/p297/Solution.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+class Solver {
+    void run(InputReader in, PrintWriter out) throws IOException {
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; ++ i) {
+            sum += in.nextInt();
+        }
+        out.println(sum % m);
+        out.close();
+    }
+}
+
+public class Solution {
+    public static void main(String args[]) {
+        try {
+            new Solver().run(new InputReader(System.in), new PrintWriter(System.out));
+        } catch (IOException e) {
+        }
+    }
+}
+
+class InputReader {
+    BufferedReader reader;
+    StringTokenizer tokenizer;
+
+    InputReader(InputStream in) {
+        reader = new BufferedReader(new InputStreamReader(in));
+        tokenizer = new StringTokenizer("");
+    }
+
+    String nextToken() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+}

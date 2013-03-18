adf2271a0bb8a254353baf94eab493fd9dcc99ce
ftiasch <ftiasch0@gmail.com>
2013-03-18 13:22:55 +0800

    added java template

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    InputReader reader;
+    PrintWriter writer;
+
+    Main() {
+        reader = new InputReader();
+        writer = new PrintWriter(System.out);
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    public void run() {
+        try {
+            // main
+        } catch (IOException ex) {
+        }
+        writer.close();
+    }
+}
+
+class InputReader {
+    BufferedReader reader;
+    StringTokenizer tokenizer;
+
+    InputReader() {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        tokenizer = new StringTokenizer("");
+    }
+
+    String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    Integer nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+}

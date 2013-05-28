a27fb3061476f8f63a828d262aee23523861c56d
ftiasch <ftiasch0@gmail.com>
2013-05-28 20:12:50 +0800

    fix

--- codeforces/31-codeforces-beta-round-31/B.py
+++ codeforces/31-codeforces-beta-round-31/B.py
@@ -1,7 +1,7 @@
 # Codeforces Beta Round #31
 # Problem B -- Sysadmin Bob
 tokens = raw_input().split('@')
-if len(tokens[0]) >= 1 and len(tokens[-1]) >= 1 and (len(tokens) <= 2 or min(map(len, tokens[1:-1])) >= 2):
+if len(tokens) >= 2 and len(tokens[0]) >= 1 and len(tokens[-1]) >= 1 and (len(tokens) <= 2 or min(map(len, tokens[1:-1])) >= 2):
     address = [] 
     for i in reversed(xrange(len(tokens) - 1)):
         address.append((tokens[i][-1] if i > 0 else tokens[0]) + '@' + tokens[i + 1])

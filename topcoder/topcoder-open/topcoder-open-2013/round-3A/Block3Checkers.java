1a0753705b5697f3530e182fe0bad481cfe8827b
ftiasch <ftiasch0@gmail.com>
2013-06-09 16:06:58 +0800

    TCO 13 Round 3A

---
+++ topcoder/topcoder-open/topcoder-open-2013/round-3A/Block3Checkers.java
@@ -0,0 +1,241 @@
+import java.math.*;
+import java.util.*;
+
+public class Block3Checkers {
+    int n, m;
+    int[] cost;
+    ArrayList[] graph;
+
+    int getid(int x, int y) {
+        return x * m + y + 3;
+    }
+
+    final static int[] DELTA_X = {-1, -1, -1,  0, 0,  1, 1, 1};
+    final static int[] DELTA_Y = {-1,  0,  1, -1, 1, -1, 0, 1};
+
+    @SuppressWarnings("unchecked")
+    public int blockThem(String[] board) {
+        n = board.length;
+        m = board[0].length();
+        cost = new int[n * m + 3];
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < m; ++ j) {
+                if (board[i].charAt(j) == 'A') {
+                    cost[getid(i, j)] = 100;
+                }
+                if (board[i].charAt(j) == '.') {
+                    cost[getid(i, j)] = 1;
+                }
+            }
+        }
+        graph = new ArrayList[n * m + 3];
+        for (int i = 0; i < n * m + 3; ++ i) {
+            graph[i] = new ArrayList <Integer>();
+        }
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < m; ++ j) {
+                for (int k = 0; k < 8; ++ k) {
+                    int x = i + DELTA_X[k];
+                    int y = j + DELTA_Y[k];
+                    if (0 <= x && x < n && 0 <= y && y < m) {
+                        graph[getid(i, j)].add(getid(x, y));
+                    }
+                }
+            }
+        }
+        if (true) {
+            int i = 0;
+            int j = 0;
+            int dx = 1;
+            int dy = 0;
+            int side = 0;
+            for (int _ = 0; _ < (n + m - 2) * 2; ++ _) {
+                if (board[i].charAt(j) == 'A') {
+                    side = (side + 1) % 3;
+                }
+                graph[side].add(getid(i, j));
+                graph[getid(i, j)].add(side);
+                if (i + dx < 0 || i + dx >= n || j + dy < 0 || j + dy >= m) {
+                    int tmp = dx;
+                    dx = -dy;
+                    dy = tmp;
+                }
+                i += dx;
+                j += dy;
+            }
+        }
+        int[][] minCost = new int[1 << 3][n * m + 3];
+        for (int[] row : minCost) {
+            Arrays.fill(row, 100);
+        }
+        for (int i = 0; i < 3; ++ i) {
+            minCost[1 << i][i] = 0;
+        }
+        for (int mask = 0; mask < 1 << 3; ++ mask) {
+            boolean[] visit = new boolean[n * m + 3];
+            Arrays.fill(visit, true);
+            Queue <Integer> queue = new LinkedList <Integer>();
+            for (int i = 0; i < n * m + 3; ++ i) {
+                for (int a = mask; a > 0; a = (a - 1) & mask) {
+                    minCost[mask][i] = Math.min(minCost[mask][i], minCost[a][i] + minCost[mask ^ a][i] - cost[i]);
+                }
+                queue.offer(i);
+            }
+            int[] distance = minCost[mask];
+            while (!queue.isEmpty()) {
+                int u = queue.poll();
+                visit[u] = false;
+                for (Object obj : graph[u]) {
+                    int v = (Integer)obj;
+                    if (distance[u] + cost[v] < distance[v]) {
+                        distance[v] = distance[u] + cost[v];
+                        if (!visit[v]) {
+                            visit[v] = true;
+                            queue.offer(v);
+                        }
+                    }
+                }
+            }
+        }
+        int answer = 100;
+        for (int i = 0; i < n * m + 3; ++ i) {
+            answer = Math.min(answer, minCost[7][i]);
+        }
+        return answer;
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			Block3CheckersHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				Block3CheckersHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class Block3CheckersHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static boolean compareOutput(int expected, int result) { return expected == result; }
+	static String formatResult(int res) {
+		return String.format("%d", res);
+	}
+	
+	static int verifyCase(int casenum, int expected, int received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.println("PASSED");
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			String[] board            = {"A......A",
+ "...N.N..",
+ ".NNN.NN.",
+ "NNNN.N.N",
+ "N.NN.NNN",
+ ".NNN.NNN",
+ "NNN...NN",
+ ".NN.A..N"};
+			int expected__            = 1;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}
+		case 1: {
+			String[] board            = {".....A",
+ "......",
+ "......",
+ "NNNNNN",
+ "A.....",
+ "A....."};
+			int expected__            = 100;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}
+		case 2: {
+			String[] board            = {"A.N",
+ "NNA",
+ "AN."};
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}
+		case 3: {
+			String[] board            = {"......NA",
+ ".NNNN.N.",
+ ".N......",
+ "....NNNN",
+ "........",
+ ".N..NNN.",
+ "......N.",
+ "A.N....A"}
+;
+			int expected__            = 3;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}
+
+		// custom cases
+
+/*      case 4: {
+			String[] board            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}*/
+/*      case 5: {
+			String[] board            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}*/
+/*      case 6: {
+			String[] board            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new Block3Checkers().blockThem(board));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE
---
+++ topcoder/topcoder-open/topcoder-open-2013/round-3A/TrickyInequality.cpp
@@ -0,0 +1,295 @@
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <list>
+#include <deque>
+#include <utility>
+#include <set>
+#include <map>
+using namespace std;
+
+const int M = 100;
+
+int power_sum[M + 1], coefficient[M + 1], binom[M + 1][M + 1], division[M + 1][M + 1];
+
+struct TrickyInequality {
+    static const int MOD = (int)1e9 + 7;
+
+    int inverse(int a) {
+        return a == 1 ? 1 : (long long)(MOD - MOD / a) * inverse(MOD % a) % MOD;
+    }
+
+    int power(int a, int n) {
+        int ret = 1;
+        while (n) {
+            if (n % 2 == 1) {
+                ret = (long long)ret * a % MOD;
+            }
+            a = (long long)a * a % MOD;
+            n >>= 1;
+        }
+        return ret;
+    }
+
+    int choose(int n, int k) {
+        if (k > n) {
+            return 0;
+        }
+        int ret = 1;
+        for (int i = 0; i < k; ++ i) {
+            ret = (long long)(n + MOD - i) % MOD * ret % MOD * inverse(i + 1) % MOD;
+        }
+        return ret;
+    }
+
+    int countSolutions(long long s, int t, int n, int m) {
+        m -= n;
+        memset(power_sum, 0, sizeof(power_sum));
+        for (int i = 1; i <= t; ++ i) {
+            int power = 1;
+            for (int j = 0; j <= m; ++ j) {
+                (power_sum[j] += power) %= MOD;
+                power = (long long)power * (MOD - i) % MOD;
+            }
+        }
+        memset(coefficient, 0, sizeof(coefficient));
+        coefficient[0] = 1;
+        for (int i = 0; i < m; ++ i) {
+            for (int j = m; j >= 0; -- j) {
+                coefficient[j] = (long long)coefficient[j] * (MOD - i) % MOD;
+                if (j) {
+                    (coefficient[j] += coefficient[j - 1]) %= MOD;
+                }
+                coefficient[j] = (long long)coefficient[j] * inverse(i + 1) % MOD;
+            }
+        }
+        memset(binom, 0, sizeof(binom));
+        for (int i = 0; i <= m; ++ i) {
+            binom[i][0] = 1;
+            for (int j = 1; j <= i; ++ j) {
+                binom[i][j] = (binom[i - 1][j - 1] + binom[i - 1][j]) % MOD;
+            }
+        }
+        memset(division, 0, sizeof(division));
+        division[0][0] = 1;
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < m; ++ j) {
+                for (int x = 1; j + x <= m; ++ x) {
+                    (division[i + 1][j + x] += (long long)division[i][j] * binom[j + x][x] % MOD * power_sum[x] % MOD) %= MOD;
+                }
+            }
+        }
+        int answer = 0;
+        for (int k = 0; k <= m; ++ k) {
+            if (coefficient[k] == 0) {
+                continue;
+            }
+            for (int j = 0; j <= m; ++ j) {
+                int b = choose(n, j);
+                if (b == 0) {
+                    continue;
+                }
+                for (int i = 0; i <= k; ++ i) {
+                    int ret = (long long)coefficient[k] * b % MOD * binom[k][i] % MOD * division[j][k - i] % MOD * power(t, n - j) % MOD * power(s % MOD, i) % MOD;
+                    (answer += ret) %= MOD;
+                }
+            }
+        }
+        return answer;
+    }
+};
+
+// BEGIN CUT HERE
+namespace moj_harness {
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			cerr << "No test cases run." << endl;
+		} else if (correct < total) {
+			cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+		} else {
+			cerr << "All " << total << " tests passed!" << endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, clock_t elapsed) { 
+		cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		cerr << verdict;
+		if (!info.empty()) {
+			cerr << " (";
+			for (int i=0; i<(int)info.size(); ++i) {
+				if (i > 0) cerr << ", ";
+				cerr << info[i];
+			}
+			cerr << ")";
+		}
+		cerr << endl;
+		
+		if (verdict == "FAILED") {
+			cerr << "    Expected: " << expected << endl; 
+			cerr << "    Received: " << received << endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			long long s               = 3;
+			int t                     = 1;
+			int n                     = 1;
+			int m                     = 2;
+			int expected__            = 2;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			long long s               = 5;
+			int t                     = 2;
+			int n                     = 2;
+			int m                     = 3;
+			int expected__            = 8;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			long long s               = 30;
+			int t                     = 1;
+			int n                     = 1;
+			int m                     = 10;
+			int expected__            = 10015005;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			long long s               = 2000;
+			int t                     = 20;
+			int n                     = 100;
+			int m                     = 200;
+			int expected__            = 35422605;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			long long s               = 999999999999999999LL;
+			int t                     = 100000;
+			int n                     = 999999900;
+			int m                     = 1000000000;
+			int expected__            = 90919453;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 5: {
+			long long s               = 999999999999999999LL;
+			int t                     = 100000;
+			int n                     = 1000000000;
+			int m                     = 1000000000;
+			int expected__            = 297793005;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 6: {
+			long long s               = ;
+			int t                     = ;
+			int n                     = ;
+			int m                     = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 7: {
+			long long s               = ;
+			int t                     = ;
+			int n                     = ;
+			int m                     = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 8: {
+			long long s               = ;
+			int t                     = ;
+			int n                     = ;
+			int m                     = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = TrickyInequality().countSolutions(s, t, n, m);
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+ 
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(atoi(argv[i]));
+	}
+}
+// END CUT HERE

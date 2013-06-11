a6aa167a5a4c7b2e41e95f79ac4eb8242f7860cb
ftiasch <ftiasch0@gmail.com>
2013-06-12 00:23:52 +0800

    SRM 576 EASY && MEDIUM

---
+++ topcoder/single-round-match/single-round-match-576/ArcadeManao.java
@@ -0,0 +1,219 @@
+import java.math.*;
+import java.util.*;
+
+public class ArcadeManao {
+    int n, m;
+    int[] parent;
+
+    int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
+    }
+
+    boolean merge(int i, int j) {
+        if (find(i) == find(j)) {
+            return false;
+        }
+        parent[find(i)] = find(j);
+        return true;
+    }
+
+    int id(int i, int j) {
+        return i * m + j;
+    }
+
+    boolean check(String[] level, int length, int x0, int y0) {
+        parent = new int[n * m];
+        for (int i = 0; i < n * m; ++ i) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 1; j < m; ++ j) {
+                if (level[i].charAt(j) == 'X' && level[i].charAt(j - 1) == 'X') {
+                    merge(id(i, j), id(i, j - 1));
+                }
+            }
+        }
+        for (int x = 1; x <= length; ++ x) {
+            for (int i = 0; i + x < n; ++ i) {
+                for (int j = 0; j < m; ++ j) {
+                    if (level[i].charAt(j) == 'X' && level[i + x].charAt(j) == 'X') {
+                        merge(id(i, j), id(i + x, j));
+                    }
+                }
+            }
+        }
+        return find(id(x0, y0)) == find(id(n - 1, 0));
+    }
+
+    public int shortestLadder(String[] level, int coinRow, int coinColumn) {
+        n = level.length;
+        m = level[0].length();
+        int low = 0;
+        int high = n - 1;
+        while (low < high) {
+            int middle = low + high >> 1;
+            if (check(level, middle, coinRow - 1, coinColumn - 1)) {
+                high = middle;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return high;
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			ArcadeManaoHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				ArcadeManaoHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class ArcadeManaoHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static boolean compareOutput(int expected, int result) { return expected == result; }
+	static String formatResult(int res) {
+		return String.format("%d", res);
+	}
+	
+	static int verifyCase(int casenum, int expected, int received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.println("PASSED");
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			String[] level            = {"XXXX....",
+ "...X.XXX",
+ "XXX..X..",
+ "......X.",
+ "XXXXXXXX"};
+			int coinRow               = 2;
+			int coinColumn            = 4;
+			int expected__            = 2;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}
+		case 1: {
+			String[] level            = {"XXXX",
+ "...X",
+ "XXXX"};
+			int coinRow               = 1;
+			int coinColumn            = 1;
+			int expected__            = 1;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}
+		case 2: {
+			String[] level            = {"..X..",
+ ".X.X.",
+ "X...X",
+ ".X.X.",
+ "..X..",
+ "XXXXX"};
+			int coinRow               = 1;
+			int coinColumn            = 3;
+			int expected__            = 4;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}
+		case 3: {
+			String[] level            = {"X"};
+			int coinRow               = 1;
+			int coinColumn            = 1;
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}
+		case 4: {
+			String[] level            = {"XXXXXXXXXX",
+ "...X......",
+ "XXX.......",
+ "X.....XXXX",
+ "..XXXXX..X",
+ ".........X",
+ ".........X",
+ "XXXXXXXXXX"};
+			int coinRow               = 1;
+			int coinColumn            = 1;
+			int expected__            = 2;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}
+
+		// custom cases
+
+/*      case 5: {
+			String[] level            = ;
+			int coinRow               = ;
+			int coinColumn            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}*/
+/*      case 6: {
+			String[] level            = ;
+			int coinRow               = ;
+			int coinColumn            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}*/
+/*      case 7: {
+			String[] level            = ;
+			int coinRow               = ;
+			int coinColumn            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new ArcadeManao().shortestLadder(level, coinRow, coinColumn));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE
---
+++ topcoder/single-round-match/single-round-match-576/TheExperiment.java
@@ -0,0 +1,205 @@
+import java.math.*;
+import java.util.*;
+
+public class TheExperiment {
+    final static int MOD = (int)1e9 + 9;
+
+    String concate(String[] strings) {
+        StringBuffer buffer = new StringBuffer("");
+        for (String string : strings) {
+            buffer.append(string);
+        }
+        return buffer.toString();
+    }
+
+    int connect(int t, boolean b) {
+        if (t == 2 || b) {
+            return 2;
+        }
+        return 1;
+    }
+
+    public int countPlacements(String[] intensities, int m, int l, int a, int b) {
+        String intensity = concate(intensities);
+        int n = intensity.length();
+        int[] sum = new int[n + 1];
+        for (int i = n - 1; i >= 0; -- i) {
+            sum[i] = sum[i + 1] + (intensity.charAt(i) - '0');
+        }
+        int[][][] ways = new int[n + 1][m + 1][3];
+        ways[0][0][0] = 1;
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j <= m; ++ j) {
+                for (int t = 0; t < 3; ++ t) {
+                    if (t != 1) {
+                        ways[i + 1][j][0] += ways[i][j][t];
+                        ways[i + 1][j][0] %= MOD;
+                    }
+                    for (int x = 1; x <= l && i + x <= n; ++ x) {
+                        int s = sum[i] - sum[i + x];
+                        if (a <= s && s <= b && j + 1 <= m) {
+                            ways[i + x][j + 1][connect(t, x == l)] += ways[i][j][t];
+                            ways[i + x][j + 1][connect(t, x == l)] %= MOD;
+                        }
+                    }
+                }
+            }
+        }
+        return (ways[n][m][0] + ways[n][m][2]) % MOD;
+    }
+
+    void debug(Object...os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			TheExperimentHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				TheExperimentHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class TheExperimentHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static boolean compareOutput(int expected, int result) { return expected == result; }
+	static String formatResult(int res) {
+		return String.format("%d", res);
+	}
+	
+	static int verifyCase(int casenum, int expected, int received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.println("PASSED");
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			String[] intensity        = {"341156"};
+			int M                     = 3;
+			int L                     = 3;
+			int A                     = 6;
+			int B                     = 10;
+			int expected__            = 2;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}
+		case 1: {
+			String[] intensity        = {"999112999"};
+			int M                     = 2;
+			int L                     = 4;
+			int A                     = 21;
+			int B                     = 30;
+			int expected__            = 2;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}
+		case 2: {
+			String[] intensity        = {"111"};
+			int M                     = 2;
+			int L                     = 2;
+			int A                     = 2;
+			int B                     = 3;
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}
+		case 3: {
+			String[] intensity        = {"59059", "110", "1132230231"};
+			int M                     = 1;
+			int L                     = 5;
+			int A                     = 10;
+			int B                     = 20;
+			int expected__            = 6;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}
+		case 4: {
+			String[] intensity        = {"111111111111111111111111", "111111111111111111111111111", "11111111111111111111111"};
+			int M                     = 12;
+			int L                     = 8;
+			int A                     = 4;
+			int B                     = 2700;
+			int expected__            = 418629948;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}
+
+		// custom cases
+
+/*      case 5: {
+			String[] intensity        = ;
+			int M                     = ;
+			int L                     = ;
+			int A                     = ;
+			int B                     = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}*/
+/*      case 6: {
+			String[] intensity        = ;
+			int M                     = ;
+			int L                     = ;
+			int A                     = ;
+			int B                     = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}*/
+/*      case 7: {
+			String[] intensity        = ;
+			int M                     = ;
+			int L                     = ;
+			int A                     = ;
+			int B                     = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new TheExperiment().countPlacements(intensity, M, L, A, B));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE

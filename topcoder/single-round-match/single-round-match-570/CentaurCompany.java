dfd815950ffc9ce1ede6e2450de42c6c2fd6e288
ftiasch <ftiasch0@gmail.com>
2013-06-20 22:34:50 +0800

    SRM 570

---
+++ topcoder/single-round-match/single-round-match-570/CentaurCompany.java
@@ -0,0 +1,210 @@
+import java.math.*;
+import java.util.*;
+
+@SuppressWarnings("unchecked")
+public class CentaurCompany {
+    final static double EPS = 1e-12;
+
+    int n;
+    ArrayList[] trees;
+
+    double[][][] ways;
+
+    void solve(int p, int u) {
+        ways[u][0][0 + n] = ways[u][1][1 + n] = 0.5;
+        for (Object obj : trees[u]) {
+            int v = (Integer)obj;
+            if (v != p) {
+                solve(u, v);
+                double[][] newWays = new double[2][2 * n + 1];
+                for (int uType = 0; uType < 2; ++ uType) {
+                    for (int uCount = -n; uCount <= n; ++ uCount) {
+                        if (Math.abs(ways[u][uType][uCount + n]) < EPS) {
+                            continue;
+                        }
+                        for (int vType = 0; vType < 2; ++ vType) {
+                            for (int vCount = -n; vCount <= n; ++ vCount) {
+                                if (Math.abs(ways[v][vType][vCount + n]) < EPS) {
+                                    continue;
+                                }
+                                int delta = vCount;
+                                if (uType == 1 && vType == 1) {
+                                    delta -= 2;
+                                }
+                                newWays[uType][uCount + delta + n] += ways[u][uType][uCount + n] * ways[v][vType][vCount + n];
+                            }
+                        }
+                    }
+                }
+                ways[u] = newWays;
+            }
+        }
+    }
+
+    public double getvalue(int[] a, int[] b) {
+        n = a.length + 1;
+        trees = new ArrayList[n];
+        for (int i = 0; i < n; ++ i) {
+            trees[i] = new ArrayList <Integer>();
+        }
+        for (int i = 0; i < n - 1; ++ i) {
+            a[i] --;
+            b[i] --;
+            trees[a[i]].add(b[i]);
+            trees[b[i]].add(a[i]);
+        }
+        ways = new double[n][2][2 * n + 1];
+        solve(-1, 0);
+        double answer = 0;
+        for (int type = 0; type < 2; ++ type) {
+            for (int count = -n; count <= n; ++ count) {
+                answer += ways[0][type][count + n] * Math.max(count - 2, 0);
+            }
+        }
+        return answer * 2;
+    }
+
+    void debug(Object...os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			CentaurCompanyHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				CentaurCompanyHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class CentaurCompanyHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static final double MAX_DOUBLE_ERROR = 1E-9;
+	static boolean compareOutput(double expected, double result){ if(Double.isNaN(expected)){ return Double.isNaN(result); }else if(Double.isInfinite(expected)){ if(expected > 0){ return result > 0 && Double.isInfinite(result); }else{ return result < 0 && Double.isInfinite(result); } }else if(Double.isNaN(result) || Double.isInfinite(result)){ return false; }else if(Math.abs(result - expected) < MAX_DOUBLE_ERROR){ return true; }else{ double min = Math.min(expected * (1.0 - MAX_DOUBLE_ERROR), expected * (1.0 + MAX_DOUBLE_ERROR)); double max = Math.max(expected * (1.0 - MAX_DOUBLE_ERROR), expected * (1.0 + MAX_DOUBLE_ERROR)); return result > min && result < max; } }
+	static double relativeError(double expected, double result) { if (Double.isNaN(expected) || Double.isInfinite(expected) || Double.isNaN(result) || Double.isInfinite(result) || expected == 0) return 0; return Math.abs(result-expected) / Math.abs(expected); }
+	
+	static String formatResult(double res) {
+		return String.format("%.10g", res);
+	}
+	
+	static int verifyCase(int casenum, double expected, double received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.print("PASSED");
+			double rerr = relativeError(expected, received);
+			if (rerr > 0) System.err.printf(" (relative error %g)", rerr);
+			System.err.println();
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			int[] a                   = {1};
+			int[] b                   = {2};
+			double expected__         = 0.0;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}
+		case 1: {
+			int[] a                   = {1,1,1};
+			int[] b                   = {2,3,4};
+			double expected__         = 0.125;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}
+		case 2: {
+			int[] a                   = {1,2,3,2,2};
+			int[] b                   = {2,3,4,5,6};
+			double expected__         = 0.375;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}
+		case 3: {
+			int[] a                   = {1,2,3,4,5,6,7,8,9};
+			int[] b                   = {2,3,4,5,6,7,8,9,10};
+			double expected__         = 0.41796875;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}
+		case 4: {
+			int[] a                   = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+			int[] b                   = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
+			double expected__         = 15.500000001076842;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}
+		case 5: {
+			int[] a                   = {10, 7, 2, 5, 6, 2, 4, 9, 7};
+			int[] b                   = {8, 10, 10, 4, 1, 6, 2, 2, 3};
+			double expected__         = 0.646484375;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}
+
+		// custom cases
+
+/*      case 6: {
+			int[] a                   = ;
+			int[] b                   = ;
+			double expected__         = ;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}*/
+/*      case 7: {
+			int[] a                   = ;
+			int[] b                   = ;
+			double expected__         = ;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}*/
+/*      case 8: {
+			int[] a                   = ;
+			int[] b                   = ;
+			double expected__         = ;
+
+			return verifyCase(casenum__, expected__, new CentaurCompany().getvalue(a, b));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE
---
+++ topcoder/single-round-match/single-round-match-570/CurvyonRails.java
@@ -0,0 +1,304 @@
+import java.math.*;
+import java.util.*;
+
+public class CurvyonRails {
+    @SuppressWarnings("unchecked")
+    class Network {
+        int n;
+        ArrayList[] graph;
+
+        class Edge {
+            int u, v, capacity, cost;
+            Edge back;
+
+            Edge(int u, int v, int capacity, int cost) {
+                this.u = u;
+                this.v = v;
+                this.capacity = capacity;
+                this.cost = cost;
+            }
+
+            public String toString() {
+                return String.format("%d->%d %d %d", u, v, capacity, cost);
+            }
+        }
+
+        Network(int n) {
+            this.n = n;
+            graph = new ArrayList[n];
+            for (int i = 0; i < n; ++ i) {
+                graph[i] = new ArrayList <Edge>();
+            }
+        }
+
+        void addEdge(int u, int v, int capacity, int cost) {
+//System.err.println(String.format("%d->%d", u, v));
+            Edge e = new Edge(u, v, capacity, cost);
+            Edge f = new Edge(v, u, 0, -cost);
+            e.back = f;
+            graph[u].add(e);
+            f.back = e;
+            graph[v].add(f);
+        }
+
+        int[] maxCostMaxFlow(int source, int target) {
+            int flow = 0;
+            int cost = 0;
+            Edge[] back = new Edge[n];
+            int[] distance = new int[n];
+            boolean[] visit = new boolean[n];
+            while (true) {
+                Arrays.fill(distance, Integer.MIN_VALUE);
+                distance[source] = 0;
+                Arrays.fill(visit, false);
+                Queue <Integer> queue = new LinkedList <Integer>();
+                queue.offer(source);
+                while (!queue.isEmpty()) {
+                    int u = queue.poll();
+                    visit[u] = false;
+                    for (Object obj : graph[u]) {
+                        Edge e = (Edge)obj;
+                        if (e.capacity > 0 && distance[u] + e.cost > distance[e.v]) {
+                            back[e.v] = e;
+                            distance[e.v] = distance[u] + e.cost;
+                            if (!visit[e.v]) {
+                                visit[e.v] = true;
+                                queue.offer(e.v);
+                            }
+                        }
+                    }
+                }
+                if (distance[target] == Integer.MIN_VALUE) {
+                    break;
+                }
+                flow ++;
+                for (int i = target; i != source; i = back[i].back.v) {
+                    Edge e = back[i];
+                    cost += e.cost;
+                    e.capacity --;
+                    e.back.capacity ++;
+                }
+            }
+            return new int[]{flow, cost};
+        }
+    }
+    
+    int n, m;
+
+    int getID(int level, int x, int y) {
+        return (x * m + y) * 4 + level;
+    }
+
+    final static int[] DELTA_X = { 0, 0, -1, 1};
+    final static int[] DELTA_Y = {-1, 1,  0, 0};
+
+    public int getmin(String[] field) {
+        n = field.length;
+        m = field[0].length();
+        int count = 0;
+        int cCount = 0;
+        Network network = new Network(n * m * 4 + 2);
+        int source = n * m * 4;
+        int target = source + 1;
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < m; ++ j) {
+                if (field[i].charAt(j) == 'w') {
+                    continue;
+                }
+                count ++;
+                boolean ok = field[i].charAt(j) == 'C';
+                if (ok) {
+                    cCount ++;
+                }
+                if ((i + j) % 2 == 0) {
+                    int x = getID(2, i, j);
+                    network.addEdge(source, x, 2, 0);
+                    for (int t = 0; t < 2; ++ t) {
+                        network.addEdge(x, getID(t, i, j), 1, 0);
+                        network.addEdge(x, getID(t, i, j), 1, ok ? 1 : 0);
+                    }
+                    for (int k = 0; k < 4; ++ k) {
+                        int ii = i + DELTA_X[k];
+                        int jj = j + DELTA_Y[k];
+                        if (0 <= ii && ii < n && 0 <= jj && jj < m) {
+                            int t = k / 2;
+                            network.addEdge(getID(t, i, j), getID(t, ii, jj), 1, 0);
+                        }
+                    }
+                } else {
+                    int x = getID(3, i, j);
+                    for (int t = 0; t < 2; ++ t) {
+                        network.addEdge(getID(t, i, j), x, 1, 0);
+                        network.addEdge(getID(t, i, j), x, 1, ok ? 1 : 0);
+                    }
+                    network.addEdge(x, target, 2, 0);
+                }
+            }
+        }
+        int[] ret = network.maxCostMaxFlow(source, target);
+        if (ret[0] != count) {
+            return -1;
+        }
+        return cCount * 2 - ret[1];
+    }
+
+    void debug(Object...os) {
+        System.err.println(Arrays.deepToString(os));
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			CurvyonRailsHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				CurvyonRailsHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class CurvyonRailsHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static boolean compareOutput(int expected, int result) { return expected == result; }
+	static String formatResult(int res) {
+		return String.format("%d", res);
+	}
+	
+	static int verifyCase(int casenum, int expected, int received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.println("PASSED");
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			String[] field            = {".."
+,".."};
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+		case 1: {
+			String[] field            = {"wCCww"
+,"wCC.."
+,"..w.."
+,"....w"
+,"ww..w"};
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+		case 2: {
+			String[] field            = {"C.w"
+,"..."
+,".C."};
+			int expected__            = 1;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+		case 3: {
+			String[] field            = {"."};
+			int expected__            = -1;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+		case 4: {
+			String[] field            = {"w"};
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+		case 5: {
+			String[] field            = {"CC..CCCw.CwC..CC.w.C",
+ "C.CCCwCCC.w.w..C.w..",
+ "wwww...CC.wC.Cw.CC..",
+ "CC..CC.w..w.C..CCCC.",
+ "CC.CCC..CwwCCC.wCC..",
+ "w.C..wwCC.CC.wwwCC..",
+ ".CC.CC..CCC..CC.CC.C",
+ "Cw....C.C.CCC...CC..",
+ "CC.C..Cww.C.CwwwC..w",
+ "wCCww..C...CCCCCCC.w",
+ "C.CCw.CC.ww...C.CCww",
+ "C.C.C.CCwCC..wCCw.Cw",
+ "CCC.C...w..C.wC.wCCw",
+ "CC.C..C..CCC.CC.C...",
+ "C.ww...CCC..CC...CCC",
+ "...CCC.CwwwC..www.C.",
+ "wwCCCCC.w.C.C...wCwC",
+ "CCwC.CwCCC.C.w.Cw...",
+ "C.w.wC.CC.CCC.C.w.Cw",
+ "CCw.CCC..C..CC.CwCCw",
+ "C.wwwww.CwwCCwwwwwww"};
+			int expected__            = 9;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+
+		// custom cases
+
+      case 6: {
+			String[] field            = {
+"CCC",
+"CCC",
+"CCC"
+};
+			int expected__            = -1;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}
+/*      case 7: {
+			String[] field            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}*/
+/*      case 8: {
+			String[] field            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new CurvyonRails().getmin(field));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE

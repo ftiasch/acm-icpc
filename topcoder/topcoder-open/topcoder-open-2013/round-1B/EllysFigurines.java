2f049bf7ef1c3048451931056b595710e60a6e9b
ftiasch <ftiasch0@gmail.com>
2013-06-21 22:04:05 +0800

    TCO 13 Round 1B

---
+++ topcoder/topcoder-open/topcoder-open-2013/round-1B/EllysFigurines.java
@@ -0,0 +1,191 @@
+import java.util.*;
+import java.math.*;
+
+public class EllysFigurines {
+    int count(int mask, int n, int k) {
+        int ret = 0;
+        int last = 0;
+        for (int i = 0; i < n; ++ i) {
+            if (i >= last && (mask >> i & 1) == 1) {
+                ret ++;
+                last = i + k;
+            }
+        }
+        return ret;
+    }
+
+    public int getMoves(String[] board, int r, int c) {		
+        int n = board.length;
+        int m = board[0].length();
+        int answer = Integer.MAX_VALUE;
+        for (int mask = 0; mask < 1 << m; ++ mask) {
+            int rowMask = 0;
+            for (int i = 0; i < n; ++ i) {
+                for (int j = 0; j < m; ++ j) {
+                    if (board[i].charAt(j) == 'X' && (mask >> j & 1) == 0) {
+                        rowMask |= 1 << i;
+                    }
+                }
+            }
+            answer = Math.min(answer, count(rowMask, n, r) + count(mask, m, c));
+        }
+        return answer;
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			EllysFigurinesHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				EllysFigurinesHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class EllysFigurinesHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static boolean compareOutput(int expected, int result) { return expected == result; }
+	static String formatResult(int res) {
+		return String.format("%d", res);
+	}
+	
+	static int verifyCase(int casenum, int expected, int received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.println("PASSED");
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			String[] board            = {".X.X.",
+ "XX..X",
+ ".XXX.",
+ "...X.",
+ ".X.XX"};
+			int R                     = 1;
+			int C                     = 2;
+			int expected__            = 3;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}
+		case 1: {
+			String[] board            = {".X.X.",
+ "XX..X",
+ ".X.X.",
+ "...X.",
+ ".X.XX"};
+			int R                     = 2;
+			int C                     = 2;
+			int expected__            = 2;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}
+		case 2: {
+			String[] board            = {"XXXXXXX"};
+			int R                     = 2;
+			int C                     = 3;
+			int expected__            = 1;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}
+		case 3: {
+			String[] board            = {"XXXXX",
+ "X....",
+ "XXX..",
+ "X....",
+ "XXXXX"};
+			int R                     = 1;
+			int C                     = 1;
+			int expected__            = 4;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}
+		case 4: {
+			String[] board            = {"XXX..XXX..XXX.",
+ ".X..X....X...X",
+ ".X..X....X...X",
+ ".X..X....X...X",
+ ".X...XXX..XXX.",
+ "..............",
+ "...XX...XXX...",
+ "....X......X..",
+ "....X....XXX..",
+ "....X......X..",
+ "...XXX..XXX..."};
+			int R                     = 1;
+			int C                     = 2;
+			int expected__            = 7;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}
+
+		// custom cases
+
+/*      case 5: {
+			String[] board            = ;
+			int R                     = ;
+			int C                     = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}*/
+/*      case 6: {
+			String[] board            = ;
+			int R                     = ;
+			int C                     = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}*/
+/*      case 7: {
+			String[] board            = ;
+			int R                     = ;
+			int C                     = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new EllysFigurines().getMoves(board, R, C));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE
---
+++ topcoder/topcoder-open/topcoder-open-2013/round-1B/EllysReversals.java
@@ -0,0 +1,181 @@
+import java.util.*;
+import java.math.*;
+
+public class EllysReversals {
+    void process(int[][] c, String a, int d) {
+        int n = a.length();
+        for (int i = 0; i + 1 < n; i += 2) {
+            int x = a.charAt(i) - 'a';
+            int y = a.charAt(i + 1) - 'a';
+            if (x <= y) {
+                c[x][y] += d;
+            } else {
+                c[y][x] += d;
+            }
+        }
+    }
+
+    boolean equal(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+        int n = a.length();
+        if (n % 2 == 1 && a.charAt(n - 1) != b.charAt(n - 1)) {
+            return false;
+        }
+        int[][] count = new int[26][26];
+        process(count, a, 1);
+        process(count, b, -1);
+        for (int i = 0; i < 26; ++ i) {
+            for (int j = 0; j < 26; ++ j) {
+                if (count[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int getMin(String[] words) {		
+        int n = words.length;
+        int answer = 0;
+        boolean[] visit = new boolean[n];
+        for (int i = 0; i < n; ++ i) {
+            if (visit[i]) {
+                continue;
+            }
+            int size = 0;
+            for (int j = 0; j < n; ++ j) {
+                if (!visit[j] && equal(words[i], words[j])) {
+                    size ++;
+                    visit[j] = true;
+                }
+            }
+            answer += size % 2;
+        }
+        return answer;
+    }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+		if (args.length == 0) {
+			EllysReversalsHarness.run_test(-1);
+		} else {
+			for (int i=0; i<args.length; ++i)
+				EllysReversalsHarness.run_test(Integer.valueOf(args[i]));
+		}
+	}
+// END CUT HERE
+}
+
+// BEGIN CUT HERE
+class EllysReversalsHarness {
+	public static void run_test(int casenum) {
+		if (casenum != -1) {
+			if (runTestCase(casenum) == -1)
+				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = runTestCase(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			System.err.println("No test cases run.");
+		} else if (correct < total) {
+			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+		} else {
+			System.err.println("All " + total + " tests passed!");
+		}
+	}
+	
+	static boolean compareOutput(int expected, int result) { return expected == result; }
+	static String formatResult(int res) {
+		return String.format("%d", res);
+	}
+	
+	static int verifyCase(int casenum, int expected, int received) { 
+		System.err.print("Example " + casenum + "... ");
+		if (compareOutput(expected, received)) {
+			System.err.println("PASSED");
+			return 1;
+		} else {
+			System.err.println("FAILED");
+			System.err.println("    Expected: " + formatResult(expected)); 
+			System.err.println("    Received: " + formatResult(received)); 
+			return 0;
+		}
+	}
+
+	static int runTestCase(int casenum__) {
+		switch(casenum__) {
+		case 0: {
+			String[] words            = {"esprit", "god", "redotopc", "odcpoter", "dog"};
+			int expected__            = 3;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}
+		case 1: {
+			String[] words            = {"no", "zaphod", "just", "very", "improbable"};
+			int expected__            = 5;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}
+		case 2: {
+			String[] words            = {"rats", "live", "stressed", "to", "act", "as", "star", "desserts", "of", "evil", "cat", "sa", "fo", "ot"};
+			int expected__            = 0;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}
+		case 3: {
+			String[] words            = {"topcoder", "redocpot", "doretopc", "cpotdoer", "harlemshake"};
+			int expected__            = 1;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}
+		case 4: {
+			String[] words            = {"iprlzgukfggzg", "bmhxvjbrtkbxy", "khapjiabbny", "nqlwgmcyvdikt",
+ "nxromtvtpug", "leealcapovm", "ushnxwjczczbmd", "bwhykzupcux",
+ "xrlboyuwlnsp", "bbjoketeheezfs", "dxfztrldomjqkv", "dkbktqdtgfujcut",
+ "zfybzyuxgpnt", "ffmsldrdftode", "vopuufksxd", "pqhbsiujwda",
+ "yhwbkzupcux", "hkbabnapjiy", "zqsqefrrzehtxn", "yovinyguyudmv"};
+			int expected__            = 16;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}
+
+		// custom cases
+
+/*      case 5: {
+			String[] words            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}*/
+/*      case 6: {
+			String[] words            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}*/
+/*      case 7: {
+			String[] words            = ;
+			int expected__            = ;
+
+			return verifyCase(casenum__, expected__, new EllysReversals().getMin(words));
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+
+// END CUT HERE
